0d00c2c45d0a81c5901cce2184b98917757712ae
Aleksei <lexa24032@gmail.com>
2022-07-11 18:03:57 +0300

    an engine for custom select elements is launched

--- src/script/main.js
+++ src/script/main.js
@@ -1,2 +1,36 @@
 const headline = document.querySelector('#headline');
 headline.textContent = 'Set Timer';
+
+// ===============================================
+
+const customSelectBlock = document.querySelectorAll('.item-set-timer');
+customSelectBlock.forEach(function (everyCustomSelect) {
+  const customSelectButton = everyCustomSelect.querySelector('.item-set-timer__btn');
+  const customSelectList = everyCustomSelect.querySelector('.item-set-timer__list');
+  const customSelectItem = customSelectList.querySelectorAll('.list-set-timer__item');
+
+  customSelectButton.addEventListener('click', function () {
+    customSelectList.classList.toggle('_visible');
+    this.classList.add('_active');
+  });
+  customSelectItem.forEach(function (item) {
+    item.addEventListener('click', function (e) {
+      e.stopPropagation();
+      customSelectButton.innerText = this.innerText;
+      customSelectButton.focus();
+      customSelectList.classList.remove('_visible');
+    });
+  });
+  document.addEventListener('click', function (e) {
+    if (e.target !== customSelectButton) {
+      customSelectButton.classList.remove('_active');
+      customSelectList.classList.remove('_visible');
+    }
+  });
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Tab' || e.key === 'Escape') {
+      customSelectButton.classList.remove('_active');
+      customSelectList.classList.remove('_visible');
+    }
+  });
+});
